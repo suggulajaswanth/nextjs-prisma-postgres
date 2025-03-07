8402c853006398ba0d451ddf15d49f2dd16249dd
Jaswanth Suggula <j.suggula@f5.com>
2025-03-07 14:07:20 +0530

    feat: disable type check

--- app/api/add-post/route.ts
+++ app/api/add-post/route.ts
@@ -1,20 +1,28 @@
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-export async function POST(request: { json: () => any; }){
-    const res = await request.json()
-    const {title, content} = res;
-     const result = await prisma.post.create({
-        data: {
-            title,
-            content,
-            published: true,
-            author: {create: {
-                name: 'jas'
-            }}
-        }
-     })
+export async function POST(request: Request) { 
+    try {
+        const { title, content } = await request.json();
 
-     console.log("result",res)
-    return NextResponse.json({res})
+        const result = await prisma.post.create({
+            data: {
+                title,
+                content,
+                published: true,
+                author: {
+                    create: {
+                        name: "jas",
+                    },
+                },
+            },
+        });
+
+        return NextResponse.json({ result }, { status: 201 }); 
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to create post" },
+            { status: 500 }
+        ); 
+    }
 }
--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -2,13 +2,11 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function DELETE(
-    request: Request, 
-    context: { params: { id: string } }
+    request: Request,
+    { params }: { params: { id: string } }
 ) {
-    const { params } = context;  
-    const id = params.id;  
-
-    console.log('params', id);
+    const { id } = params;
+    console.log("id", id);
 
     if (!id) {
         return NextResponse.json({ error: "Missing ID" }, { status: 400 });
@@ -16,10 +14,16 @@ export async function DELETE(
 
     try {
         await prisma.post.delete({
-            where: { id }
+            where: { id },
         });
-        return NextResponse.json({ message: "Post deleted successfully" });
+
+        return NextResponse.json({ message: "Post deleted successfully" }, { status: 200 });
     } catch (error) {
+        console.error("Failed to delete post:", error);
+        if (error instanceof Error && error.message.includes("Record to delete does not exist")) {
+            return NextResponse.json({ error: "Post not found" }, { status: 404 });
+        }
+
         return NextResponse.json({ error: "Failed to delete post" }, { status: 500 });
     }
 }
